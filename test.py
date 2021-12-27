5b4b6fd6ebb758c473248d15f33732870edafa7f
rijusmit <rijusmit.biswas@gmail.com>
2021-12-27 22:57:32 +0530

    bot

--- test.py
+++ test.py
@@ -17,8 +17,7 @@ mycursor.execute("select phrase,n_phrase from statement")
 list_phrase=list()
 myresult=mycursor.fetchall()
 
-client= discord.Client()
-
+client= commands.Bot(command_prefix="#")
 
 @client.event
 async def on_ready():
@@ -32,18 +31,8 @@ async def on_message(message):
     gg=find(lambda x:x[0]==message.content,myresult)
     print((gg[1]))
     await message.channel.send(gg[1])
-    """if message.content.startswith("&hello"):
-        await message.channel.send("Hello")
-    if message.content.startswith("&hi"):
-        await message.channel.send("Kamon Achhish")
-    if message.content.startswith("&good"):
-        await message.channel.send("Bhalo achhi")"""
-async def kick(ctx, member : discord.member,*,reason=None):
-    await member.kick(reason=reason)
-    await ctx.send(f'User{member} has been kicked')
-async def ban(ctx, member : discord.member,*,reason=None):
-    await member.ban(reason=reason)
-    await ctx.send(f'User{member} has been banned')
+
+
 
 
 
